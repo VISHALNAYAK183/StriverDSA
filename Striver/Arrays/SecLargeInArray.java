dc6bd94489dd7aa0c2e816d249227cf509ec0d7a
vishal <vishunyk108@gmail.com>
2026-02-26 15:47:12 +0530

    Striver

--- Striver/Arrays/SecLargeInArray.java
+++ Striver/Arrays/SecLargeInArray.java
@@ -3,18 +3,33 @@ package Striver.Arrays;
 import java.util.Scanner;
 
 
+// class SLargeE{
+//     int SLEle(int[] arr){
+//         int largest=arr[0]; int seclargest=-1;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]>largest){
+//                 seclargest=largest;
+//                 largest=arr[i];
+                
+//             }
+//         }
+//         return seclargest;
+//     }
+// }
+
 class SLargeE{
     int SLEle(int[] arr){
-        int largest=arr[0]; int seclargest=-1;
+        int largest=arr[0]; int seclarge=-1;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]>largest){
-                seclargest=largest;
-                largest=arr[i];
-                
+                if(arr[i]>largest){
+                    seclarge=largest;
+                    largest=arr[i];
+                } else if (arr[i] > seclarge && arr[i] != largest) {
+                seclarge = arr[i];
             }
         }
-        return seclargest;
-    }
+        return seclarge;
+  }
 }
 public class SecLargeInArray {
      public static void main(String[] args){
